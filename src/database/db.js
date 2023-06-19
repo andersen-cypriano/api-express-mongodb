617fc29bdfcec33a18ba2ecdc54a958bb77ecc47
andersen-cypriano <87385956+andersen-cypriano@users.noreply.github.com>
2023-06-19 00:05:10 -0300

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 async function connectDatabase() {
   await mongoose.connect(
-    'mongodb+srv://acypriano:W26f6FcqJs0THDdk@clusterinstafeed.66edeiw.mongodb.net/?retryWrites=true&w=majority',
+    '',
   )
 }
 
